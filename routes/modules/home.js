66423c86780bf4f446e8a8ccd6b3c6eb7d83707a
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-06-30 16:20:51 +0800

    feat: add basic views

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('this is the first sentences')
+  res.render('index')
 })
 
 module.exports = router
