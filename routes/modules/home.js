421293cc8064615d6da15cf041961a6b4f2bfc3d
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-03 11:18:49 +0800

    feat: modify index.hbs

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const Item = require('../../models/items')
+const items = require('../../models/items')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  Item.find()
+    .lean()
+    .then(items => res.render('index', { items }))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
