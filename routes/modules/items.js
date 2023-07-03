39a8d0bcfd390181722a581733a9d7a25a241936
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-03 11:34:44 +0800

    feat: add create todo function

--- routes/modules/items.js
+++ routes/modules/items.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const items = require('../../models/items')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const name = req.body.name
+  return items.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
