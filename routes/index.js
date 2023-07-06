d093e6ed32b4a9e5f73d6adbcbc3a17ed67c9553
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-06 23:48:59 +0800

    feat: add login & register page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const items = require('./modules/items')
+const users = require('./modules/users') 
 
-
+router.use('/users', users)
 router.use('/items', items)
 router.use('/', home)
 
