942f70863d52548765f5dd8db8e716c82aa3cb2b
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-07 15:00:40 +0800

    feat: add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const items = require('./modules/items')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
 router.use('/items', authenticator, items)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
