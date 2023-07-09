601765d22f2f0f73429afd5de1bf1cbbba82010c
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-09 23:54:41 +0800

    feat: revise Itemseeder (add SEED_USER)

--- models/seeds/itemSeeder.js
+++ models/seeds/itemSeeder.js
@@ -1,13 +1,36 @@
+const bcrypt = require('bcryptjs')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const Item = require('../item')
+const User = require('../user')
 const db = require('../../config/mongoose')
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  for (let i = 0; i < 10; i++) {
-    Item.create({ name: `item-${i}` })
-  }
-  console.log('seeders done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Item.create({ name: `name-${i}`, userId })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
