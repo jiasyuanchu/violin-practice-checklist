1e7c2780e2252db06904948706af9c6c08c7f889
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-17 19:39:53 +0800

    feat: main.hbs revised/ add composers router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const composers = require('./modules/composers')
 const home = require('./modules/home')
 const items = require('./modules/items')
 const users = require('./modules/users')
@@ -9,6 +10,7 @@ const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/composers', authenticator, composers)
 router.use('/items', authenticator, items)
 router.use('/auth', auth)
 router.use('/', authenticator, home)
