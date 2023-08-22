3bba9c8051f4f90d56aa1f4a8264f0891a1c09d0
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-22 13:29:47 +0800

    delete: remove composer modules

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const composers = require('./modules/composers')
 const home = require('./modules/home')
 const items = require('./modules/items')
 const users = require('./modules/users')
@@ -10,7 +9,6 @@ const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/composers', authenticator, composers)
 router.use('/items', authenticator, items)
 router.use('/auth', auth)
 router.use('/', authenticator, home)
