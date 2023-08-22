57b1317e9e36b4aaaba8290dfde4efe6352d7ae4
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-22 13:44:19 +0800

    revise PORT

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
